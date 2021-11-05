5ea377c5cbab052d3b33f6a044cc872324455912
Roberto Silva <beto-frs@hotmail.com>
2021-11-04 23:55:17 -0300

    Exercicio concluido

---
+++ Aulas/Aula10/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using static System.Console;
+
+namespace Aula10
+{
+    class Program
+    {
+        static int qtd = 10;
+        static int[] valores, indicePar, indiceImpar, valorPar, valorImpar;
+
+        static void Main(string[] args)
+        {
+            Vetor();
+            Inverter(valores);
+            VetorIndice(valores);
+            VetorElemento(valores);
+        }
+
+        static void Vetor()
+        {
+            valores = new int[qtd];
+            Random rand = new Random();
+            for (int i = 0; i < qtd; i++)
+            {
+                valores[i] = rand.Next(1, 10);
+                Write(" {0} |", valores[i]);
+            }
+            Write(" => Vetor Principal\n\n");
+        }
+
+        static void Inverter(int[] vetor)
+        {
+            int[] inverso = new int[qtd];
+            int indice = 0;
+            for (int i = qtd; i > 0; i--)
+            {
+                inverso[indice] = valores[(i-1)];
+                Write(" {0} |", inverso[indice]);
+                indice++;
+            }
+            Write(" => Vetor Invertido\n\n");
+        }
+
+        static void VetorIndice(int[] vetor)
+        {
+            indicePar = new int[qtd];
+            indiceImpar = new int[qtd];
+            int par = 0, impar = 0;
+            for (int i = 0; i < qtd; i++)
+            {
+                if (i%2==0)
+                {
+                    indicePar[par] = valores[i];
+                    par++;
+                }
+                else
+                {
+                    indiceImpar[impar] = valores[i];
+                    impar++;
+                }
+            }
+            ExibirVetores(indicePar, indiceImpar, "vetores");
+        }
+
+        static void VetorElemento(int[] vetor)
+        {
+            valorPar = new int[qtd];
+            valorImpar = new int[qtd];
+            int par = 0, impar = 0;
+            for (int i = 0; i < qtd; i++)
+            {
+                if (valores[i]%2==0)
+                {
+                    valorPar[par] = valores[i];
+                    par++;
+                }
+                else
+                {
+                    valorImpar[impar] = valores[i];
+                    impar++;
+                }
+            }
+            ExibirVetores(valorPar, valorImpar, "vetores");
+        }
+
+        static void ExibirVetores(int[] pares, int[] impares, string info)
+        {
+            foreach (var indice in pares)
+            {
+                if (indice != 0)
+                {
+                    Write(" {0} |", indice);
+                }
+            }
+            Write(" => Vetor com {0} pares\n\n",info);
+            foreach (var indice in impares)
+            {
+                if (indice != 0)
+                {
+                    Write(" {0} |", indice);
+                }
+            }
+            Write(" => Vetor com {0} ímpares\n\n",info);
+        }
+
+    }
+}
--- Front/JavaScript/script.js
+++ Front/JavaScript/script.js
@@ -1,3 +1,4 @@
+/*
 alert('Hello World')
 
 var objeto = 
@@ -27,7 +28,7 @@ else
     alert('False');
 }
 
-
+*/
 
 // var numero = 1;
 // if (numero == "1") { //compara os valores/conteúdos
@@ -60,12 +61,184 @@ else
 // for (let i = 0; i < alunos.length; i++) {
 //     alert(alunos[i]);
 // }
-var texto = "";
+/*var texto = "";
 var numero = 1;
 while (numero < 10) {
     texto += numero + " ";
     numero++;
 }
 alert(texto);
+*/
+/*for (let i = 0; i<10;i++){
+    if(i%2==0){
+        alert(i+' é um número par');
+    }
+}
+
+var total=0;
+for (let i = 0; i<=100;i++)
+{
+ total += i;   
+}
+alert(total);*/
 
 
+
+// var numero = 1;
+
+// if (numero == "1") { //compara os valores/conteúdos
+//     alert('True');
+// }
+// else {
+//     alert('False');
+// }
+
+// if (numero === "1") {//compara o tipo e o valor (ambos devem ser iguais)
+//     alert('True');
+// }
+// else {
+//     alert('False');
+// }
+
+
+
+// var suco = 3;
+
+// switch (suco) {
+//     case 1:
+//         alert('fazer 1 copo de suco');
+//         break;
+//     case 2:
+//         alert('fazer meia jarra de suco');
+//         break;
+//     case 3:
+//         alert('Fazer uma jarra de suco');
+//         break;
+//     default:
+//         alert('opção default');
+// }
+
+// var alunos = ["Rodrigo", "Eduardo", "Thiago", "Pablo", "Francisco"];
+
+// for (let i = 0; i < alunos.length; i++) {
+//     alert(alunos[i]);
+// }
+
+
+// var texto = "";
+// var numero = 1;
+
+// while (numero < 10) {
+//     texto += numero + " ";
+//     numero++;
+// }
+
+// alert(texto);
+
+
+// for (let i = 0; i <= 10; i++) 
+// {
+//     if(i%2==0)
+//     {
+//         alert(i);
+//     }
+// }
+
+
+// var total = 0;
+
+// for (let i = 0; i <= 100; i++) {
+//     total += i;
+// }
+
+// alert(total);
+
+
+
+
+function minhafuncao() 
+{
+    alert('Hello!');
+}
+
+
+function mostrarPares() 
+{
+    for (let i = 0; i <= 10; i++) 
+    {
+        if(i%2==0)
+        {
+            alert(i);
+        }
+    }
+} 
+ 
+function mostrarSomatorio() 
+{
+    var total = 0;
+
+    for (let i = 0; i <= 100; i++) 
+    {
+        total += i;
+    }
+
+    alert(total);
+}
+
+function deixarcaixaalta()
+{
+    var x = document.getElementById("nome");
+    x.value = x.value.toUpperCase();
+    // document.getElementById("nome").value = document.getElementById("nome").valeu.toUpperCase();
+}
+
+function mostrarDados()
+{
+    var nome = document.getElementById("nome").value;
+    var idade = document.getElementById("idade").value;
+
+    alert(nome + " tem " + idade + " anos!");
+}
+
+function verificarIdade()
+{
+    var nome = document.getElementById("nome").value;
+    var idade = document.getElementById("idade").value;
+
+    if(idade >= 18)
+    {
+        alert(nome + " é maior de idade!");
+    }
+    else
+    {
+        alert(nome + " não é maior de idade!")
+    }
+}
+
+function somar()
+{
+    var valor1 = parseInt(document.getElementById("valor1").value);
+    var valor2 = parseInt(document.getElementById("valor2").value);
+
+    var soma = valor1 + valor2;
+    console.log(soma);
+    alert(soma);
+}
+
+function somarcor(botao)
+{
+    var valor1 = parseInt(document.getElementById("valor1").value);
+    var valor2 = parseInt(document.getElementById("valor2").value);
+
+    var soma = valor1 + valor2;
+
+    if(soma %2 ==0)
+    {
+        // document.getElementById("btn").style.background = '#F00';
+        botao.style.background = '#F00';
+    }
+    else
+    {
+        botao.style.background = '#00F';
+    }
+}
